9ba25722a6c8d40aa733952f4dd430fb1130fa3c
Kenan Abo Zaineddin <124913244+kenan-azd-dev@users.noreply.github.com>
2024-09-08 12:41:17 +0300

    feat: configure factories for Job and Employer models

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,13 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => fake()->jobTitle(),
+            'location' => fake()->randomElement(['Remote', 'On-Site', 'Hybrid']),
+            'salary' => fake()->randomElement(['$25,000 USD', '$50,000 USD', '$75,000 USD', '$100,000 USD', '$125,000 USD']),
+            'schedule' => fake()->randomElement(['Full Time', 'Part Time']),
+            'url' => fake()->url(),
+            'featured' => false,
         ];
     }
 }
